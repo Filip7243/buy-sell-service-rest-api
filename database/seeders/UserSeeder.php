a8adc2636310b9903c582a8c54b52bb08d84270b
Filip <filip7243@gmail.com>
2023-05-02 18:15:00 +0200

    made some seeds

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            [
+                'first_name' => 'John',
+                'last_name' => 'Doe',
+                'email' => 'john@mail.com',
+                'password' => Hash::make('1234')
+            ],
+            [
+                'first_name' => 'Mike',
+                'last_name' => 'Scott',
+                'email' => 'mike@mail.com',
+                'password' => Hash::make('1234')
+            ],
+        ]);
     }
 }
