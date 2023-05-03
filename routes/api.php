6621e00733f5b22f47e183f0121e1461cc6697d3
Filip <filip7243@gmail.com>
2023-05-03 14:49:32 +0200

    added routes for categories

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\V1\AuthController;
+use App\Http\Controllers\V1\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,9 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthController::class, 'user']);
     Route::get('logout', [AuthController::class, 'logout']);
+
+//    Route::get('categories', [CategoryController::class, 'index']);
+//    Route::get('categories/{id}', [CategoryController::class, 'show']);
+    Route::apiResource('categories', CategoryController::class);
 });
 
