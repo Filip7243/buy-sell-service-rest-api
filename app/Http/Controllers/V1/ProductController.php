6cf1cddf68f20b02d56354b844547ad5cbab54dd
Filip <filip7243@gmail.com>
2023-05-11 11:30:35 +0200

    added basic method for search and adding products

--- app/Http/Controllers/V1/ProductController.php
+++ app/Http/Controllers/V1/ProductController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\V1\StoreProductRequest;
+use App\Http\Resources\V1\ProductCollection;
+use App\Http\Resources\V1\ProductResource;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return new ProductCollection(Product::paginate());
+    }
+
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    public function store(StoreProductRequest $request)
+    {
+        return new ProductResource(Product::create($request->all()));
+    }
 }
