8d82b9c5d9d8808f42733d15d22d26e5902ea717
Filip <filip7243@gmail.com>
2023-05-11 11:30:03 +0200

    modified realtionships functin

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable
 
     public function product()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Product::class, 'product_id', 'id');
     }
 
 }
