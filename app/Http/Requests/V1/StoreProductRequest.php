a75490fb6651e3950dbedbba1d1fc02d3297db4f
Filip <filip7243@gmail.com>
2023-05-11 11:31:36 +0200

    added requests and resources for products

---
+++ app/Http/Requests/V1/StoreProductRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'description' => 'required',
+//            'image' => 'image|mimes:jpeg,png,jpg|size:1024',
+            'image' => 'required',
+            'price' => 'required|decimal:1,6',
+            'quantity' => 'required',
+            'product_condition' => 'required|in:NEW,USED',
+            'type' => 'required|in:FOR_SALE,TO_BUY',
+            'category_id' => 'required|exists:App\Models\Category,id',
+            'user_id' => 'required|exists:App\Models\User,id'
+        ];
+    }
+}
---
+++ app/Http/Resources/V1/ProductResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'image' => $this->image,
+            'price' => $this->price,
+            'quantity' => $this->quantity,
+            'product_condition' => $this->product_condition,
+            'type' => $this->type,
+            'categories' => CategoryResource::collection($this->categories),
+            'user' => $this->users // TODO: make resource for user!
+        ];
+    }
+}
