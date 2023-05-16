ab324445f249c97a4555842bd3570dd24ba5a311
Filip <filip7243@gmail.com>
2023-05-16 17:24:03 +0200

    creted requests for store and update orders

---
+++ app/Http/Requests/V1/StoreOrderRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'price' => 'required|decimal:1,6',
+            'order_status' => 'required|in:PAID,NOT_PAID',
+            'product_id' => 'required|exists:App\Models\Product,id',
+            'user_id' => 'required|exists:App\Models\User,id'
+        ];
+
+        //TODO: in update too |unique:App\Models\Product,id
+    }
+}
