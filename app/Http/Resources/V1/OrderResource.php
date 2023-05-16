2f158eebfe9c5c3efdfc086c4435b7d470b67b44
Filip <filip7243@gmail.com>
2023-05-16 16:46:06 +0200

    added resources for order

---
+++ app/Http/Resources/V1/OrderResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'price' => $this->price,
+            'order_status' => $this->order_status,
+            'product' => $this->product->id,
+            'user' => $this->users->id
+        ];
+    }
+}
