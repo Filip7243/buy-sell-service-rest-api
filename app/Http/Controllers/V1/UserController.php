cc6e2fbd93fd544c49bb89f04549bcdcb709182b
Filip <filip7243@gmail.com>
2023-05-17 17:19:02 +0200

    added another CRUD methods

--- app/Http/Controllers/V1/UserController.php
+++ app/Http/Controllers/V1/UserController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\UpdateUserRequest;
 use App\Http\Resources\V1\UserCollection;
 use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
@@ -20,8 +22,25 @@ class UserController extends Controller
         return new UserResource($user);
     }
 
-    public function update(UpdateUserRequest $request)
+    public function update(UpdateUserRequest $request, User $user)
     {
+        $user->update($request->all());
 
+        return response(['User has been updated successfully!' => new UserResource($user)]);
     }
+
+    public function destroy($id)
+    {
+        if ($user = User::find($id)) {
+            $user->delete();
+
+            return response([
+                'message' => 'User deleted'
+            ], Response::HTTP_NO_CONTENT);
+        }
+
+        return response(['message' => 'User with id: ' . $id . ' not found!'],
+            Response::HTTP_NOT_FOUND);
+    }
+
 }
