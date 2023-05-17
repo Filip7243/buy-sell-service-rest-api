bfcb5f47d13d30a259209b222ec0cda3ef558d53
Filip <filip7243@gmail.com>
2023-05-17 17:05:02 +0200

    added UserController

---
+++ app/Http/Controllers/V1/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\UserCollection;
+use App\Http\Resources\V1\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return new UserCollection(User::paginate());
+    }
+
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    public function update(UpdateUserRequest $request)
+    {
+
+    }
+}
