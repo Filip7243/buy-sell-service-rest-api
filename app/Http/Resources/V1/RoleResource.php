433e24650a9c5235391a598848fcee3d3c2eebd8
Filip <filip7243@gmail.com>
2023-05-17 18:22:26 +0200

    added role resources

--- app/Http/Resources/V1/RoleResource.php
+++ app/Http/Resources/V1/RoleResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources\V1;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource
+class RoleResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,11 +14,6 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'first_name' => $this->first_name,
-            'last_name' => $this->last_name,
-            'email' => $this->email,
-            'roles' => RoleResource::collection($this->roles)
-        ];
+        return ['name' => $this->name];
     }
 }
