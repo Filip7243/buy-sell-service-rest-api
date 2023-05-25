2025640537036c27bcebbe099f8a198788c137a7
Filip <filip7243@gmail.com>
2023-05-25 12:22:22 +0200

    added new functions

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,13 +2,25 @@
 
 namespace App\Services;
 
+use App\Http\Resources\V1\OrderCollection;
+use App\Http\Resources\V1\ProductCollection;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserService
 {
-
-    public function findUserOrders(User $user)
+    public function findUserOrders(User $user) : OrderCollection
     {
-        return $user->order;
+        return new OrderCollection($user->orders);
+    }
+
+    public function findUserProducts(User $user) : ProductCollection
+    {
+        $userProducts = DB::table('products')
+            ->selectRaw('*')
+            ->where('user_id', $user->id)
+            ->get();
+
+        return new ProductCollection($userProducts);
     }
 }
